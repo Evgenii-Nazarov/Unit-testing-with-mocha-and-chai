1c75042a29b42d615c11a7f7b3ee2f886cb62209
Evgenii Nazarov <ev66647260@gmail.com>
2020-01-10 22:01:02 -0800

    eslint init

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
-function isAdult (age) {
-    const error = new Error('wrong age');
-    if ( {}.toString.call(age) !== '[object Number]'  ) return undefined;
-    if (age > 17) return true;
-    else if (age <= 0) return error;
-    else return false;
+function isAdult(age) {
+  const error = new Error('wrong age');
+  if ({}.toString.call(age) !== '[object Number]') return undefined;
+  if (age > 17) return true;
+  else if (age <= 0) return error;
+  else return false;
 }
 
-function sortArrayUp (array= []) {
-    const error = new Error ('Invalid type of array\'s values');
+function sortArrayUp(array = []) {
+  const error = new Error('Invalid type of array\'s values');
 
-    if ({}.toString.call(array) !== '[object Array]') return undefined;
+  if ({}.toString.call(array) !== '[object Array]') return undefined;
 
-    for (const el of array) {
-        if ( {}.toString.call(el) !== '[object Number]' || Number.isNaN(el) ) return error;
-    };
+  for (const el of array) {
+    if ({}.toString.call(el) !== '[object Number]' || Number.isNaN(el)) return error;
+  }
+    
 
-    return array.sort((a,b) => a-b)
+  return array.sort((a, b) => a - b);
 }
 
-function isPalindrome (word) {
-    const error = new Error('Wrong input');
+function isPalindrome(word) {
+  const error = new Error('Wrong input');
 
-    if ( Number.isNaN(word) ) return error;
+  if (Number.isNaN(word)) return error;
 
-    if ( typeof(word) === 'number' ) {
-        return String(word) === String(word).split('').reverse().join('')
-    }
+  if (typeof (word) === 'number') {
+    return String(word) === String(word).split('').reverse().join('');
+  }
 
-    if ({}.toString.call(word) !== '[object String]') return error;
+  if ({}.toString.call(word) !== '[object String]') return error;
 
-    return word === word.split('').reverse().join('')
+  return word === word.split('').reverse().join('');
 }
 
-function isOddNumber (number) {
-    const error = new Error('Wrong input');
+function isOddNumber(number) {
+  const error = new Error('Wrong input'); 
 
-    if (Number.isNaN(number) || {}.toString.call(number) !== '[object Number]') return error;
+  if (Number.isNaN(number) || {}.toString.call(number) !== '[object Number]') return error;
 
-    return number % 2 === 0 ? false: true;
+  return number % 2 === 0 ? false : true;
 }
 
-function isEvenNumber (number) {
-    const error = new Error('Wrong input');
+function isEvenNumber(number) {
+  const error = new Error('Wrong input');
 
-    if (Number.isNaN(number) || {}.toString.call(number) !== '[object Number]') return error;
+  if (Number.isNaN(number) || {}.toString.call(number) !== '[object Number]') return error;
 
-    return number % 2 === 0 ? true: false;
+  return number % 2 === 0 ? true : false;
 }
 
-module.exports = {isAdult,sortArrayUp,isPalindrome,isOddNumber,isEvenNumber};
+module.exports = {isAdult, sortArrayUp, isPalindrome, isOddNumber, isEvenNumber};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,195 +1,195 @@
 //const assert = require ('assert');
-const {expect} = require ('chai');
-const {isAdult,sortArrayUp,isPalindrome, isOddNumber, isEvenNumber} = require('../index.js');
+const {expect} = require('chai');
+const {isAdult, sortArrayUp, isPalindrome, isOddNumber, isEvenNumber} = require('../index.js');
 
-describe ('testing isAdult function', () => {
+describe('testing isAdult function', () => {
 
-    it("with 18 argument should be true",() => {
-        expect(isAdult(18),  ).to.be.true;
-    });
+  it('with 18 argument should be true', () => {
+    expect(isAdult(18),).to.be.true;
+  });
 
-    it("with 17 argument should be false",() => {
-        expect(isAdult(17) ).to.be.false;
-    });
+  it('with 17 argument should be false', () => {
+    expect(isAdult(17)).to.be.false;
+  });
 
-    it("with no argument age should be undefined",() => {
-        expect( isAdult() ).to.be.undefined;
-    });
+  it('with no argument age should be undefined', () => {
+    expect(isAdult()).to.be.undefined;
+  });
 
-    it("with string argument age should be undefined",() => {
-        expect(isAdult('18') ).to.be.undefined;
-    });
+  it('with string argument age should be undefined', () => {
+    expect(isAdult('18')).to.be.undefined;
+  });
 
-    it("with array argument age should be undefined",() => {
-        expect(isAdult([18]) ).to.be.undefined;
-    });
+  it('with array argument age should be undefined', () => {
+    expect(isAdult([18])).to.be.undefined;
+  });
 
-    it("with object argument age should be undefined",() => {
-        expect(isAdult({age:18}) ).to.be.undefined;
-    });
+  it('with object argument age should be undefined', () => {
+    expect(isAdult({age: 18})).to.be.undefined;
+  });
 
-    it("with argument below or equal 0 should be wrong age",() => {
-        expect(isAdult(-12) ).to.be.an('error');
-    });
-
-});
-
-describe ('testing sortArrayUp function', () => {
-    it('with [4,2,1,3,0] array should be [0,1,2,3,4]', () => {
-        expect(  sortArrayUp([4,2,1,3,0])).to.deep.equal([0,1,2,3,4])
-    });
-
-    it('with string array should be invalid element inside the array', () => {
-        expect(  sortArrayUp(['for','two','one','three','zero'])).to.be.an('error')
-    });
-
-    it('with NaN el in array should be invalid element inside the array', () => {
-        expect(  sortArrayUp([2,3,NaN,4,5])).to.be.an('error')
-    });
-
-    it('with Null el in array should be invalid element inside the array', () => {
-        expect(  sortArrayUp([2,3,null,4,5])).to.be.an('error')
-    });
-
-    it('with Undefined el in array should be invalid element inside the array', () => {
-        expect(  sortArrayUp([2,3,undefined,4,5])).to.be.an('error')
-    });
-
-    it('with [] array should be []', () => {
-        expect(  sortArrayUp([])).to.deep.equal([])
-    });
-
-    it('with no argument should be []', () => {
-        expect(  sortArrayUp()).to.deep.equal([])
-    });
-
-    it('with object argument array should be undefined', () => {
-        expect(  sortArrayUp({ a: 1, b: 2} )).to.be.undefined;
-    });
-
-    it('with string argument should be undefined', () => {
-        expect(  sortArrayUp('4,2,1,3,0')).to.be.undefined;
-    });
-
-    it('with number argument should be undefined', () => {
-        expect(  sortArrayUp(42130)).to.be.undefined;
-    });
-
+  it('with argument below or equal 0 should be wrong age', () => {
+    expect(isAdult(-12)).to.be.an('error');
+  });
 
 });
 
-describe ('testing isPalindrome function', () => {
+describe('testing sortArrayUp function', () => {
+  it('with [4,2,1,3,0] array should be [0,1,2,3,4]', () => {
+    expect(sortArrayUp([4, 2, 1, 3, 0])).to.deep.equal([0, 1, 2, 3, 4]);
+  });
 
-   it('with aba should be true', () => {
-      expect( isPalindrome('aba') ).to.be.true;
-    });
+  it('with string array should be invalid element inside the array', () => {
+    expect(sortArrayUp(['for', 'two', 'one', 'three', 'zero'])).to.be.an('error');
+  });
 
-    it('with abbaa should be false', () => {
-        expect( isPalindrome('abbaa') ).to.be.false;
-    });
+  it('with NaN el in array should be invalid element inside the array', () => {
+    expect(sortArrayUp([2, 3, NaN, 4, 5])).to.be.an('error');
+  });
 
-    it('with 121 argument should be true', () => {
-        expect( isPalindrome(121) ).to.be.true;
-    })
+  it('with Null el in array should be invalid element inside the array', () => {
+    expect(sortArrayUp([2, 3, null, 4, 5])).to.be.an('error');
+  });
 
-    it('with 123 argument should be Wrong argument', () => {
-        expect( isPalindrome(123) ).to.be.false;
-    });
+  it('with Undefined el in array should be invalid element inside the array', () => {
+    expect(sortArrayUp([2, 3, undefined, 4, 5])).to.be.an('error');
+  });
 
-    it('with Array argument should be Wrong argument', () => {
-        expect( isPalindrome(['aba']) ).to.be.an('error');
-    });
+  it('with [] array should be []', () => {
+    expect(sortArrayUp([])).to.deep.equal([]);
+  });
 
-    it('with Object argument should be Wrong argument', () => {
-        expect( isPalindrome({word:'aba'}) ).to.be.an('error');
-    });
+  it('with no argument should be []', () => {
+    expect(sortArrayUp()).to.deep.equal([]);
+  });
 
-    it('with NaN argument should be Wrong argument', () => {
-        expect( isPalindrome(NaN) ).to.be.an('error');
-    });
+  it('with object argument array should be undefined', () => {
+    expect(sortArrayUp({a: 1, b: 2})).to.be.undefined;
+  });
 
-    it('with Null argument should be Wrong argument', () => {
-        expect( isPalindrome(null) ).to.be.an('error');
-    });
+  it('with string argument should be undefined', () => {
+    expect(sortArrayUp('4,2,1,3,0')).to.be.undefined;
+  });
 
-    it('with Undefined argument should be Wrong argument', () => {
-        expect( isPalindrome(undefined) ).to.be.an('error');
-    });
+  it('with number argument should be undefined', () => {
+    expect(sortArrayUp(42130)).to.be.undefined;
+  });
+
+
+});
+
+describe('testing isPalindrome function', () => {
+
+  it('with aba should be true', () => {
+    expect(isPalindrome('aba')).to.be.true;
+  });
+
+  it('with abbaa should be false', () => {
+    expect(isPalindrome('abbaa')).to.be.false;
+  });
+
+  it('with 121 argument should be true', () => {
+    expect(isPalindrome(121)).to.be.true;
+  });
+
+  it('with 123 argument should be Wrong argument', () => {
+    expect(isPalindrome(123)).to.be.false;
+  });
+
+  it('with Array argument should be Wrong argument', () => {
+    expect(isPalindrome(['aba'])).to.be.an('error');
+  });
+
+  it('with Object argument should be Wrong argument', () => {
+    expect(isPalindrome({word: 'aba'})).to.be.an('error');
+  });
+
+  it('with NaN argument should be Wrong argument', () => {
+    expect(isPalindrome(NaN)).to.be.an('error');
+  });
+
+  it('with Null argument should be Wrong argument', () => {
+    expect(isPalindrome(null)).to.be.an('error');
+  });
+
+  it('with Undefined argument should be Wrong argument', () => {
+    expect(isPalindrome(undefined)).to.be.an('error');
+  });
 
 });
 
 describe('testing isOddNumber function', () => {
-    it('with 2 argument should be false', () => {
-        expect(isOddNumber(2)).to.be.false
-    });
+  it('with 2 argument should be false', () => {
+    expect(isOddNumber(2)).to.be.false;
+  });
 
-    it('with 1 argument should be true', () => {
-        expect(isOddNumber(1)).to.be.true
-    });
+  it('with 1 argument should be true', () => {
+    expect(isOddNumber(1)).to.be.true;
+  });
 
-    it('with no argument should be error', () => {
-        expect(isOddNumber()).to.be.an('error')
-    });
+  it('with no argument should be error', () => {
+    expect(isOddNumber()).to.be.an('error');
+  });
 
-    it('with NaN argument should be error', () => {
-        expect(isOddNumber(NaN)).to.be.an('error')
-    });
+  it('with NaN argument should be error', () => {
+    expect(isOddNumber(NaN)).to.be.an('error');
+  });
 
-    it('with Null argument should be error', () => {
-        expect(isOddNumber(null)).to.be.an('error')
-    });
+  it('with Null argument should be error', () => {
+    expect(isOddNumber(null)).to.be.an('error');
+  });
 
-    it('with Undefined argument should be error', () => {
-        expect(isOddNumber(undefined)).to.be.an('error')
-    });
+  it('with Undefined argument should be error', () => {
+    expect(isOddNumber(undefined)).to.be.an('error');
+  });
 
-    it('with string argument should be error', () => {
-        expect(isOddNumber('2')).to.be.an('error')
-    });
+  it('with string argument should be error', () => {
+    expect(isOddNumber('2')).to.be.an('error');
+  });
 
-    it('with Array argument should be error', () => {
-        expect(isOddNumber([1])).to.be.an('error')
-    });
+  it('with Array argument should be error', () => {
+    expect(isOddNumber([1])).to.be.an('error');
+  });
 
-    it('with Obj argument should be error', () => {
-        expect(isOddNumber({1:1})).to.be.an('error')
-    });
-})
+  it('with Obj argument should be error', () => {
+    expect(isOddNumber({1: 1})).to.be.an('error');
+  });
+});
 
 describe('testing isEvenNumber function', () => {
-    it('with 1 argument should be false', () => {
-        expect(isEvenNumber(1)).to.be.false
-    });
+  it('with 1 argument should be false', () => {
+    expect(isEvenNumber(1)).to.be.false;
+  });
 
-    it('with 2 argument should be true', () => {
-        expect(isEvenNumber(2)).to.be.true
-    });
+  it('with 2 argument should be true', () => {
+    expect(isEvenNumber(2)).to.be.true;
+  });
 
-    it('with no argument should be error', () => {
-        expect(isEvenNumber()).to.be.an('error')
-    });
+  it('with no argument should be error', () => {
+    expect(isEvenNumber()).to.be.an('error');
+  });
 
-    it('with NaN argument should be error', () => {
-        expect(isEvenNumber(NaN)).to.be.an('error')
-    });
+  it('with NaN argument should be error', () => {
+    expect(isEvenNumber(NaN)).to.be.an('error');
+  });
 
-    it('with Null argument should be error', () => {
-        expect(isEvenNumber(null)).to.be.an('error')
-    });
+  it('with Null argument should be error', () => {
+    expect(isEvenNumber(null)).to.be.an('error');
+  });
 
-    it('with Undefined argument should be error', () => {
-        expect(isEvenNumber(undefined)).to.be.an('error')
-    });
+  it('with Undefined argument should be error', () => {
+    expect(isEvenNumber(undefined)).to.be.an('error');
+  });
 
-    it('with string argument should be error', () => {
-        expect(isEvenNumber('2')).to.be.an('error')
-    });
+  it('with string argument should be error', () => {
+    expect(isEvenNumber('2')).to.be.an('error');
+  });
 
-    it('with Array argument should be error', () => {
-        expect(isEvenNumber([1])).to.be.an('error')
-    });
+  it('with Array argument should be error', () => {
+    expect(isEvenNumber([1])).to.be.an('error');
+  });
 
-    it('with Obj argument should be error', () => {
-        expect(isEvenNumber({1:1})).to.be.an('error')
-    });
-})
+  it('with Obj argument should be error', () => {
+    expect(isEvenNumber({1: 1})).to.be.an('error');
+  });
+});
